63202a000e4faddbe7c92c9ef9b3d4fecb840eca
scuti-loctd <106049561+scuti-loctd@users.noreply.github.com>
2022-12-06 21:54:17 +0700

    add code react ecommerce

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./index.scss";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import store from "./store/configureStore";
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  // <React.StrictMode>
+  <BrowserRouter>
+    <Provider store={store}>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        closeOnClick
+        pauseOnHover={false}
+      />
+      <App />
+    </Provider>
+  </BrowserRouter>
+  // </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4575d8",
+        blueDark: "#00005C",
+        black: "#000",
+        dark: "#222222",
+        text1: "#474747",
+        text2: "#666666",
+        text3: "#666666",
+        white: "#fff",
+        error: "#f14550",
+      },
+    },
+  },
+  plugins: [],
+};
